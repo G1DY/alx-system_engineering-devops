bfab9cd48c35e0fea15a009655d857eb679b9503
Gideon <kipkerring.gedion@gmail.com>
2023-05-03 07:14:02 -0700

    run some statistics on the TextMe app text messages transactions.

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -5,13 +5,4 @@
 # The receiver phone number or name (including country code if present)
 # The flags that were used
 
-input_file = ARGV[0]
-input_line = FILE.readlines(input_file)
-
-input_lines.each do |line|
-        fields = line.split(',')
-	sender = fields[0]
-	reciever = fields[1]
-	flags = fields[2]
-	puts "#{sender},#{reciever},#{flags}"
-end
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
