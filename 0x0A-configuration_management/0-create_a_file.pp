0e540958e00571eaa989b231b874b27c08fe6021
Gideon <kipkerring.gedion@gmail.com>
2023-06-23 07:51:31 -0700

    creates a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet to create a file in /tmp
 
 file { '/tmp/school':
+    content => 'I love Puppet',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0744',
-    content => 'I love puppet',
 }
