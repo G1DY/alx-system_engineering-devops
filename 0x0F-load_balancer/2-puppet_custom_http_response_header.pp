cb5807dd6da987de11db38decbca1520aee4a39f
Gideon <kipkerring.gedion@gmail.com>
2023-07-04 02:14:36 -0700

    creating http header using puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,8 @@
 # creating http header using puppet
-exec { 'update':
-    command => '/usr/bin/apt-get update',
-}
-package { 'nginx':
-    ensure => 'present',
-}
-file_line { 'http_header':
-    path  => '/etc/nginx/nginx.conf',
-    match => 'http {',
-    line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-}
-exec {'run':
-    command => '/usr/sbin/service nginx restart',
+exec { 'command':
+    command => 'sudo apt-get -y update;
+    sudo apt-get -y install nginx;
+    sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+    service nginx start',
+    provider => shell,
 }
