470b7a323848285b4206eb7e54f4a68a9e364ab1
Gideon <kipkerring.gedion@gmail.com>
2023-08-17 12:07:28 +0000

    Fixes DDOS

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+#fixes ddos
 exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
@@ -8,3 +9,4 @@ exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
+
